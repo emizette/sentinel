3284f020b032a1d2f05c00d3e5de50d779b955b3
Emilette <eds26@njit.edu>
2026-05-06 21:58:02 -0400

    initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+// import { Switch, Route, Link } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+      Hello world :3
+    </div>
+  );
+}
+export default App;
