2af33b8874d867216c08ec54a477ab7025aa5059
fer <mafersomort@gmail.com>
2026-05-07 23:31:01 -0400

    added home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import ChatBot from './components/ChatBot';
+import Home from './components/Home';
+import About from './components/About';
+import FAQ from './components/FAQ';
 import './App.css';
 
 function App() {
+  const [page, setPage] = useState('home');
+
   return (
     <div className="app-root">
-      <Navbar />
+      <Navbar page={page} setPage={setPage} />
       <div className="app-body">
-        <ChatBot />
+        {page === 'home'     && <Home setPage={setPage} />}
+        {page === 'sentinel' && <ChatBot />}
+        {page === 'about'    && <About />}
+        {page === 'faq'      && <FAQ />}
       </div>
     </div>
   );
